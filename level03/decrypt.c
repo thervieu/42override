b8295123b81e092b20c8747681ed1f8c531c7f9b
Thibault HERVIEU <thervieu@e2r2p18.clusters.42paris.fr>
2022-01-25 12:23:44 +0100

    lgtm

--- level03/decrypt.c
+++ level03/decrypt.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <strings.h>
+#include <string.h>
 
 int main(void) {
 	for (int i = 0; i < 21; i++) {
-		char buffer[strlen("Q}|u`sfg~sf{}|a3") + 1] = "Q}|u`sfg~sf{}|a3";
+		char buffer[20] = "Q}|u`sfg~sf{}|a3";
 		for (int j = 0; j < strlen(buffer); j++) {
 			
 			buffer[j] = buffer[j] ^ i;
